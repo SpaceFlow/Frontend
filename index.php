02f204b04be7fcba1ea82f777cac3f72124d5456
Kirschn <info@kirschn.de>
2016-02-13 15:28:07 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+// Very important header, you know
+header("HTTP/1.1 418 I'm A Teapot");
+?>
+<!doctype html>
+<html>
+<head>
+<title>I'm A Teapot</title>
+</head>
+<body>
+Nothing here, yet
+</body>
+</html>
